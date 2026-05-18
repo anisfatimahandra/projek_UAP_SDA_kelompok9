0ab577ae3a2f25a210e300967a0f4be067d3714a
anisfatimahandra <anisfatimahandra@gmail.com>
2026-05-18 11:52:24 +0700

    class

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,66 +1,84 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Mahasiswa {
-    private:
-        long long nim;      
-        string nama;        
-        string jurusan;     
+class Buku {
+protected:
+    string kategori;
+    string judul;
+    string penulis;
+    double harga;
 
-    public:
-        void setNim(long long n) {
-            nim = n;
-        }
-        void setNama(string nm) {
-            nama = nm;
-        }
-        void setJurusan(string j) {
-            jurusan = j;
-        }
+public:
+    Buku(string j, string p, double h) {
+        judul    = j;
+        penulis  = p;
+        harga    = h;
+        kategori = "Buku Fisik";
+    }
 
-        long long getNim() {
-            return nim;
-        }
-        string getNama() {
-            return nama;
-        }
-        string getJurusan() {
-            return jurusan;
-        }
+    void tampilInfo() {
+        cout << "[Kategori: " << kategori << "]" << endl;
+        cout << "Judul   : " << judul << endl;
+        cout << "Penulis : " << penulis << endl;
+        cout << "Harga   : Rp" << harga << endl;
+    }
 
-        void tampilkan() {
-            cout << "\n DATA MAHASISWA " << endl;
-            cout << "NIM     : " << nim     << endl;
-            cout << "Nama    : " << nama    << endl;
-            cout << "Jurusan : " << jurusan << endl;
-            cout << "" << endl;
-        }
+    int hitungDiskon(int persen) {
+        return harga - (harga * persen / 100);
+    }
+
+    double hitungDiskon(double persen) {
+        return harga - (harga * persen / 100.0);
+    }
+};
+
+class BukuDigital : public Buku {
+private:
+    double ukuranMB;
+
+public:
+    BukuDigital(string j, string p, double h, double mb)
+        : Buku(j, p, h) {          
+        ukuranMB = mb;
+        kategori = "Buku Digital"; 
+    }
+
+    void tampilInfo() {
+        Buku::tampilInfo();        
+        cout << "Ukuran  : " << ukuranMB << " MB" << endl;
+    }
+
+    int hitungDiskon(int persen) {
+        int hargaDiskon = harga - (harga * persen / 100);
+        return hargaDiskon + 5000; 
+    }
+
+    int hitungFaktorial(int n) {
+        if (n <= 0) return 1; 
+        return n * hitungFaktorial(n - 1); 
+    }
+
+    void hitungKombinasi(int n, int r) {
+        cout << "[Kategori: " << kategori << "]" << endl;
+        int hasil = hitungFaktorial(n) / 
+                   (hitungFaktorial(r) * hitungFaktorial(n - r));
+        cout << "Kombinasi memilih " << r << " dari " << n 
+             << " buku = " << hasil << " cara" << endl;
+    }
 };
 
 int main() {
-    Mahasiswa mhs;
+    Buku buk("Laskar Pelangi", "Andrea Hirata", 85000);
+    buk.tampilInfo();
+    cout << "Diskon 10% (int)   : Rp" << buk.hitungDiskon(10) << endl;
+    cout << "Diskon 7.5% (double): Rp" << buk.hitungDiskon(7.5) << endl;
 
-    long long nim;
-    string nama, jurusan;
+    cout << "\n BukuDigital \n";
 
-    cout << " INPUT DATA MAHASISWA " << endl;
-
-    cout << "Masukkan NIM (12 digit) : ";
-    cin >> nim;
-    cin.ignore();
-
-    cout << "Masukkan Nama           : ";
-    getline(cin, nama);
-
-    cout << "Masukkan Jurusan        : ";
-    getline(cin, jurusan);
-
-    mhs.setNim(nim);
-    mhs.setNama(nama);
-    mhs.setJurusan(jurusan);
-
-    mhs.tampilkan();
+    BukuDigital bd("Bumi", "Tere Liye", 75000, 2.4);
+    bd.tampilInfo();
+    cout << "Diskon 20% + biaya konversi: Rp" << bd.hitungDiskon(20) << endl;
+    bd.hitungKombinasi(5, 2);
 
     return 0;
 }
